9e35a1e9f89e9672730fa5caf0a8575c014b26eb
erictempleton1 <erictempleton1@gmail.com>
2014-04-14 18:13:40 -0400

    model changes

--- photo_site/models.py
+++ photo_site/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=15)
+
+    def __unicode__(self):
+        return '%s' % self.username
+
+class Images(models.Model):
+    user = models.ForeignKey(User)
+    file_name = models.CharField(max_length=140)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=140)
+    added = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '%s' % self.title
+
+    
