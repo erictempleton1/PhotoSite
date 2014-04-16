b5d77c7a2831d60a779a8843882db179cdfa9fb3
erictempleton1 <erictempleton1@gmail.com>
2014-04-16 06:43:55 -0400

    url building

--- photo_site/urls.py
+++ photo_site/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from photo_site import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<user_id>\d+)/home/$', views.user_page, name='user_page'),
 )
--- photo_site/views.py
+++ photo_site/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Testing django!!')
+
+# creates holding page for url /photos/253/home
+# any ID number returned
+def user_page(request, user_id):
+    return HttpResponse('User ID %s home page' % user_id)
