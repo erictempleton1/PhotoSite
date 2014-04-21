e1b611cc1b4638f642c8a2083e082ec97d5127da
erictempleton1 <erictempleton1@gmail.com>
2014-04-21 07:26:40 -0400

    form not quite working yet

--- photo_site/views.py
+++ photo_site/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib import messages
+from photo_site.forms import SignupForm
 
 # Create your views here.
 def test_layout(request):
@@ -15,13 +17,33 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            # ...
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            check_username = User.objects.filter(username=username)
+            check_email = User.objects.filter(email=email)
+
+            if check_username.exists():
+                messages.error(request, 'Username already in use')
+                return HttpResponseRedirect('/signup/')
+
+            if check_email.exists():
+                messages.error(request, 'Email already in use')
+                return HttpResposeRedirect('/signup/')
+
+            else:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                messages.success('Account created. Please login')
+                return HttpResponseRedirect('/login/')
+                
             return HttpResponseRedirect('main/')
 
     else:
         form = SignupForm()
 
-    return render(request, 'singup.html', {'form': form})
+    return render(request, 'signup.html', {'form': form})
 
     
 
