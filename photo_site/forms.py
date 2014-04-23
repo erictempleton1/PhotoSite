d03b172834a776fbb1ed8c4268cb146dc87b080f
erictempleton1 <erictempleton1@gmail.com>
2014-04-23 07:12:30 -0400

    working on login system. may need to write something custom to validate based on email

--- photo_site/forms.py
+++ photo_site/forms.py
@@ -4,3 +4,7 @@ class SignupForm(forms.Form):
     username = forms.CharField(max_length=100)
     email = forms.EmailField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
--- photo_site/views.py
+++ photo_site/views.py
@@ -2,8 +2,9 @@ from django.db import IntegrityError
 from django.shortcuts import render, render_to_response, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from photo_site.forms import SignupForm
+from photo_site.forms import SignupForm, LoginForm
 
 # Create your views here.
 def test_layout(request):
@@ -27,29 +28,39 @@ def signup(request):
 
             if check_email.exists():
                 messages.error(request, 'Email already in use')
-
             if len(password) < 4:
                 messages.error(request, 'Passwords must be 4 or more characters')
-
             else:
                 try:
 	                user = User.objects.create_user(username, email, password)
 	                user.save()
 	                messages.success(request, 'Account created. Please login')
-
                 except IntegrityError:
                     # above error thrown if username exists
                     messages.error(request, 'Username already in use')
                 return redirect('/main/login/')
-                
-
     else:
         form = SignupForm()
-
     return render(request, 'signup.html', {'form': form})
 
 def login(request):
-    return render(request, 'photos/login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/main/photos/')
+                else:
+                    messages.error(request, 'Account is not active')
+        else:
+            messages.error(request, 'Invalid email/password')
+    else:
+        form=LoginForm()
+    return render(request, 'photos/login.html', {'form': form})
 
 
 """
