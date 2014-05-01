b4a5666d1ede0b4b0f3e720ed860d19a000c2014
erictempleton1 <erictempleton1@gmail.com>
2014-05-01 07:44:41 -0400

    removed form title requirement

--- photo_site/forms.py
+++ photo_site/forms.py
@@ -10,5 +10,5 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
+    title = forms.CharField(max_length=50, required=False)
     file = forms.FileField()
