46aa97cd94eeb98b819f8be07ce3d3a1c1737e35
erictempleton1 <erictempleton1@gmail.com>
2014-05-13 10:41:57 -0400

    running tests

--- photo_site/tests.py
+++ photo_site/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from photo_site.models import Images
+from photo_site.forms import SignupForm, LoginForm, UploadFileForm, ChangePWForm
 
-# Create your tests here.
+class TestingImage(TestCase):
+
+    def add_user(self, username='joey', email='joey@joey.com', password='joey'):
+        return User.objects.create(username=username, email=email, password=password)
+
+    """
+    def add_image(self, file_url="testing", title="test", description="test!"):
+        return Images.objects.create(file_url=file_url, title=title, description=description)
+    """
+    
+
+    def test_user_add(self):
+        w = self.add_user()
+        self.assertTrue(isinstance(w, User))
+        self.assertEqual(w.__unicode__(), w.username)
+    
