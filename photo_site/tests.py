319d7203d9cf9e3fa0300f600f0b6e439091af1c
erictempleton1 <erictempleton1@gmail.com>
2014-05-14 21:39:34 -0400

    more testing

--- photo_site/tests.py
+++ photo_site/tests.py
@@ -1,28 +1,23 @@
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from photo_site.models import Images
 from photo_site.forms import SignupForm, LoginForm, UploadFileForm, ChangePWForm
-from photo_site.views import change_pw, user_page
+from photo_site.views import change_pw, user_page, login_user
 
 class TestingImage(TestCase):
-
-    def add_user(self):
-        user = User.objects.create_user(username='eric', email='eric@eric.com', password='eric')
-        return user
-
-    def second_user(self):
-        user = User.objects.create_user(username='eric1', email='eric@eric.com', password='eric')
-        return user
-
+    # creates json versions of db's to test against
+    fixtures = ['photo_site_views_testdata.json', 'user_model.json']
+    
     def test_user_add(self):
-        w = self.add_user()
-        q = self.second_user()
-        self.assertTrue(isinstance(w, User))
-        self.assertEqual(w.__unicode__(), w.username)
+        test_user1 = User.objects.create_user(username='eric', email='eric@eric.com', password='eric')
+        self.assertTrue(isinstance(test_user1, User))
+        self.assertEqual(test_user1.__unicode__(), test_user1.username)
 
-    def test_index(self):
-        found = resolve('/main/1/photos/')
-        self.assertEqual(found.func, user_page) 
+    def test_view(self):
+        response = self.client.get('/main/eric/photos/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('user_images' in response.context)
