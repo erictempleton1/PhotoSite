35df506d792641bdb08157bf2ff21faa920d0b23
erictempleton1 <erictempleton1@gmail.com>
2014-05-18 15:50:22 -0400

    working on password reset

--- photo_site/urls.py
+++ photo_site/urls.py
@@ -17,4 +17,6 @@ urlpatterns = patterns('',
     url(r'^remove/(?P<image_id>\d+)/(?P<image_url>\w+)', views.remove_image, name='remove_image'),
     # might need to adjust this as more admin features are added
     url(r'^user-admin/$', views.change_pw, name='change_pw'),
+    url(r'^reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', views.reset_confirm, name='reset_confirm'),
+    url(r'^reset/$', views.reset, name='reset'),
 )
