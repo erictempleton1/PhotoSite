446c638e779744816e6df2ac2d84a5e012c5f0e7
erictempleton1 <erictempleton1@gmail.com>
2014-05-26 20:24:35 -0400

    test for create user

--- photo_site/tests.py
+++ photo_site/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.test import TestCase, Client, LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -9,17 +10,33 @@ from photo_site.models import Images
 from photo_site.forms import SignupForm, LoginForm, UploadFileForm, ChangePWForm
 from photo_site.views import change_pw, user_page, login_user
 
-class TestingImage(TestCase):
+class TestingImage(LiveServerTestCase):
     # creates json versions of db's to test against
     fixtures = ['photo_site_views_testdata.json', 'user_model.json']
     
-    def test_user_add(self):
-        test_user1 = User.objects.create_user(username='eric7', email='eric@eric.com', password='eric')
-        self.assertTrue(isinstance(test_user1, User))
-        self.assertEqual(test_user1.__unicode__(), test_user1.username)
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-    def test_view(self):
-        response = self.client.get('/main/eric/photos/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('user_images' in response.context)
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_create_account(self):
+        self.browser.get(self.live_server_url + '/main/signup/')
+
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('eric67')
+
+        email_field = self.browser.find_element_by_name('email')
+        email_field.send_keys('eric67@eric.com')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('eric')
+
+        password_again_field = self.browser.find_element_by_name('password_again')
+        password_again_field.send_keys('eric')
+
+        password_again_field.send_keys(Keys.RETURN)
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Login', body.text)
         
