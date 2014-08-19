c59317fbefa7badcdf1bb87420846348c0459183
erictempleton1 <erictempleton1@gmail.com>
2014-08-18 20:14:24 -0400

    fixed naming convention for image delete

--- photo_site/views.py
+++ photo_site/views.py
@@ -196,8 +196,8 @@ def remove_image(request, image_id, image_url):
     # returns None if key doesn't exist
     # exists = mybucket.get_key('eric/images/2_ChugachMountains.jpg')
     image = Images.objects.get(id=image_id)
-    image_exists = mybucket.get_key(('%s/images/%s') % (request.user.username, image.filename))
-    thumb_exists = mybucket.get_key(('%s/thumbnails/%s') % (request.user.username, '_'.join(['2', 'thumb', image.filename[2:]])))
+    image_exists = mybucket.get_key(('%s/images/%s_%s') % (request.user.username, request.user.id, image.filename))
+    thumb_exists = mybucket.get_key(('%s/thumbnails/%s_%s_%s') % (request.user.username, request.user.id, 'thumb', image.filename))
 
     if image_exists is not None and thumb_exists is not None:
         # deletes from s3
