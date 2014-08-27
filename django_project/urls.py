6e475dd729fb6fb7839f2af869b17ad0c5769973
erictempleton1 <erictempleton1@gmail.com>
2014-08-27 06:56:27 -0400

    removed /main/ from url

--- django_project/urls.py
+++ django_project/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^main/', include('photo_site.urls')),
+    url(r'^', include('photo_site.urls')),
 )
